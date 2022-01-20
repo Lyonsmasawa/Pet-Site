28ec20cd52b0fc53df03afe59438a50bb2520730
Lewis <lewis.murgor@student.moringaschool.com>
2022-01-20 22:01:21 +0300

    add adopt pets functionality

---
+++ scripts.js
@@ -0,0 +1,39 @@
+
+    $("#adoptCat1").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptDog1").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptHamster").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptDog2").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptDog3").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptCat2").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptDog4").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("#adoptHamsters").click(function() {
+        $(".adoption-details").toggle();
+    });
+
+    $("form#adopt").submit(function(event) {
+        event.preventDefault();
+        var name = $("#name").val();
+        alert("Hello " + name + ". " + "We have received your adoption request. Please visit our offices to finalize the adoption process.");
+        $("#adopt")[0].reset();
+    });
