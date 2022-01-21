00230e6fe06a176993dc4d9aa8e6fe86777edf5d
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-01-20 16:34:21 -0800

    merge and edit complete

--- JS/script.js
+++ JS/script.js
@@ -1,63 +1,58 @@
 // scrollable navbar
-$(window).scroll(function(){
-    if($(this).scrollTop() > 100){
-        $('.nav').addClass('sticky')
-    } else{
-        $('.nav').removeClass('sticky')
-    }
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.nav').addClass('sticky')
+  } else {
+    $('.nav').removeClass('sticky')
+  }
 });
 
 // validation of the forms
 
 $(window).on("hashchange", function () {
-	if (location.hash.slice(1) == "signup") {
-		$(".page").addClass("extend");
-		$("#login").removeClass("active");
-		$("#signup").addClass("active");
-	} else {
-		$(".page").removeClass("extend");
-		$("#login").addClass("active");
-		$("#signup").removeClass("active");
-	}
+  if (location.hash.slice(1) == "signup") {
+    $(".page").addClass("extend");
+    $("#login").removeClass("active");
+    $("#signup").addClass("active");
+  } else {
+    $(".page").removeClass("extend");
+    $("#login").addClass("active");
+    $("#signup").removeClass("active");
+  }
 });
 $(window).trigger("hashchange");
 
 function validateLoginForm() {
-	var name = document.getElementById("logName").value;
-	var password = document.getElementById("logPassword").value;
+  var name = document.getElementById("logName").value;
+  var password = document.getElementById("logPassword").value;
 
-	if (name == "" || password == "") {
-		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
-		return false;
-	}
-
-	else if (password.length < 8) {
-		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
-		return false;
-	}
-	else {
-		alert("Successfully logged in");
-		return true;
-	}
+  if (name == "" || password == "") {
+    document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+    return false;
+  } else if (password.length < 8) {
+    document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+    return false;
+  } else {
+    alert("Successfully logged in");
+    return true;
+  }
 }
+
 function validateSignupForm() {
-	var mail = document.getElementById("signEmail").value;
-	var name = document.getElementById("signName").value;
-	var password = document.getElementById("signPassword").value;
+  var mail = document.getElementById("signEmail").value;
+  var name = document.getElementById("signName").value;
+  var password = document.getElementById("signPassword").value;
 
-	if (mail == "" || name == "" || password == "") {
-		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
-		return false;
-	}
-
-	else if (password.length < 8) {
-		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
-		return false;
-	}
-	else {
-		alert("Successfully signed up");
-		return true;
-	}
+  if (mail == "" || name == "" || password == "") {
+    document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+    return false;
+  } else if (password.length < 8) {
+    document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+    return false;
+  } else {
+    alert("Successfully signed up");
+    return true;
+  }
 }
 //contact form validation
 const form = document.getElementById("form");
@@ -74,13 +69,13 @@ form.addEventListener("submit", function (e) {
   result.innerHTML = "Please wait...";
 
   fetch("https://api.web3forms.com/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: json
-  })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    })
     .then(async (response) => {
       let json = await response.json();
       if (response.status == 200) {
@@ -111,11 +106,11 @@ const realFileBtn = document.getElementById("real-file");
 const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
 
-customBtn.addEventListener("click", function() {
+customBtn.addEventListener("click", function () {
   realFileBtn.click();
 });
 
-realFileBtn.addEventListener("change", function() {
+realFileBtn.addEventListener("change", function () {
   if (realFileBtn.value) {
     customTxt.innerHTML = realFileBtn.value.match(
       /[\/\\]([\w\d\s\.\-\(\)]+)$/
@@ -123,4 +118,10 @@ realFileBtn.addEventListener("change", function() {
   } else {
     customTxt.innerHTML = "No file chosen, yet.";
   }
+});
+$("form#form").submit(function (event) {
+  event.preventDefault();
+  var name = $("#fname").val();
+  alert("Hello " + name + ". " + "Thank you for the interest to sell on our site, we will respond to you in an email within 12 hours");
+  $("#form")[0].reset();
 });
